c84decb2d43c319fbff81a4b7a1c750855fada4e
sritejindia <sritejindia@gmail.com>
2021-01-26 16:27:43 +0530

    Second Project

---
+++ DemoSpringRestWebService/src/main/java/com/tej/project/DemoSpringRestWebService/DemoSpringRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tej.project.DemoSpringRestWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ DemoSpringRestWebService/src/test/java/com/tej/project/DemoSpringRestWebService/DemoSpringRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tej.project.DemoSpringRestWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
