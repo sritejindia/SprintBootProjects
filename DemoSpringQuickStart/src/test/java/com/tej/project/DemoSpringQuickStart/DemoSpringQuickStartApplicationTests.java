c360b11053c6c6f63cfd423161926a33c8d6c887
sritejindia <sritejindia@gmail.com>
2021-01-26 16:10:57 +0530

    First Commit

---
+++ DemoSpringQuickStart/src/test/java/com/tej/project/DemoSpringQuickStart/DemoSpringQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tej.project.DemoSpringQuickStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringQuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
