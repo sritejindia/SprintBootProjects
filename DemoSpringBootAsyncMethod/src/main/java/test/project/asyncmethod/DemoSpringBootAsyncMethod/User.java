84c03ea2bbad61aa5a51199a2d787f5444e4c4fd
sritejindia <sritejindia@gmail.com>
2021-02-01 09:44:41 +0530

    Async Method

---
+++ DemoSpringBootAsyncMethod/src/main/java/test/project/asyncmethod/DemoSpringBootAsyncMethod/User.java
@@ -0,0 +1,25 @@
+package test.project.asyncmethod.DemoSpringBootAsyncMethod;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private String name;
+    private String blog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+}
---
+++ DemoSpringBootAsyncMethod/src/test/java/test/project/asyncmethod/DemoSpringBootAsyncMethod/DemoSpringBootAsyncMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package test.project.asyncmethod.DemoSpringBootAsyncMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootAsyncMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
