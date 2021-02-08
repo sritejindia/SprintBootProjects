473e14e9c737f7a491be5adb87790557ac32923f
sritejindia <sritejindia@gmail.com>
2021-02-08 17:18:45 +0530

    Forms validation

---
+++ DemoSpringBootFormValidation/src/main/java/tej/springboot/formvalidation/DemoSpringBootFormValidation/DemoSpringBootFormValidationApplication.java
@@ -0,0 +1,13 @@
+package tej.springboot.formvalidation.DemoSpringBootFormValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootFormValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootFormValidationApplication.class, args);
+	}
+
+}
---
+++ DemoSpringBootFormValidation/src/test/java/tej/springboot/formvalidation/DemoSpringBootFormValidation/DemoSpringBootFormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package tej.springboot.formvalidation.DemoSpringBootFormValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootFormValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
