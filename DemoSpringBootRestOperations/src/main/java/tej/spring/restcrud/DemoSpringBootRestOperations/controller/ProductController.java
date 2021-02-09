d21ef32c20c5ebc596c67eb81363f281000627d5
sritejindia <sritejindia@gmail.com>
2021-02-09 15:35:17 +0530

    Adding Service and simplying controller, adding path variable

--- DemoSpringBootRestOperations/src/main/java/tej/spring/restcrud/DemoSpringBootRestOperations/controller/ProductController.java
+++ DemoSpringBootRestOperations/src/main/java/tej/spring/restcrud/DemoSpringBootRestOperations/controller/ProductController.java
@@ -1,8 +1,11 @@
 package tej.spring.restcrud.DemoSpringBootRestOperations.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import tej.spring.restcrud.DemoSpringBootRestOperations.model.Product;
+import tej.spring.restcrud.DemoSpringBootRestOperations.service.ProductService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,16 +13,16 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        return Arrays.asList(
-            new Product("P101","Monitor","Electronics"),
-            new Product("P102","Blanket","Household"),
-            new Product("P103","Laptop","Electronics"),
-            new Product("P104","Shirt","Fashion"),
-            new Product("P105","Pens","School")
-        );
+       return productService.getAllProducts();
     }
 
-
+    @GetMapping("/products/{pId}")
+    public Product getProduct(@PathVariable("pId") String id) {
+        return productService.getProduct(id);
+    }
 }
---
+++ DemoSpringBootRestOperations/src/main/java/tej/spring/restcrud/DemoSpringBootRestOperations/service/ProductService.java
@@ -0,0 +1,28 @@
+package tej.spring.restcrud.DemoSpringBootRestOperations.service;
+
+import org.springframework.stereotype.Service;
+import tej.spring.restcrud.DemoSpringBootRestOperations.model.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = Arrays.asList(
+            new Product("P101","Monitor","Electronics"),
+            new Product("P102","Blanket","Household"),
+            new Product("P103","Laptop","Electronics"),
+            new Product("P104","Shirt","Fashion"),
+            new Product("P105","Pens","School")
+    );
+
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    public Product getProduct(String id) {
+        return products.stream().filter(product -> product.getId().equals(id)).findFirst().get();
+    }
+
+}
