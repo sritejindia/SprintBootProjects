5bf07340a371200cf25770b18fd816291a825d7c
sritejindia <sritejindia@gmail.com>
2021-02-12 16:54:09 +0530

    Spring Security Default Login page

---
+++ DemoSpringBootSecurity/src/test/java/tej/springboot/security/DemoSpringBootSecurity/DemoSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package tej.springboot.security.DemoSpringBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
